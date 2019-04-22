2b7262171e8d2fbe92b4ec7e8de637745f2c07dc
Unknown <weisong_ong@mymail.sutd.edu.sg>
2019-04-22 21:52:26 +0800

    Updated Customer.py

--- Customer.py
+++ Customer.py
@@ -1,6 +1,6 @@
 #################################################
 # Property of EZQ
-# LAST MODIFIED: Jamesong7822 @ 190419
+# LAST MODIFIED BY: Jamesong7822 @ 030419
 #################################################
 
 #################################################
@@ -44,9 +44,9 @@ class OrderScreen(Screen):
         super(Screen, self).__init__()
         self.popup = None
 
-    def update_order(self, instance):
-        self.ids.Order.text += instance.text
-        self.Label.text += instance.text
+#    def update_order(self, instance):
+#        self.ids.Order.text += instance.text
+#        self.Label.text += instance.text
 
     def update_phone(self, instance):
         self.ids.Phone.text += instance.text
@@ -55,7 +55,12 @@ class OrderScreen(Screen):
     def keypad(self, title):
         layout = BoxLayout(orientation="vertical")
         # Generate Label
-        self.Label = Label(text="", size_hint=(1, 0.2))
+        text = ''
+        if self.ids.Phone.on_touch_down:
+            if self.ids.Phone.text != '':
+                text = self.ids.Phone.text
+
+        self.Label = Label(text=text, size_hint=(1, 0.2))
         layout.add_widget(self.Label)
 
         # Generate Keypad
@@ -87,17 +92,17 @@ class OrderScreen(Screen):
         if not self.popup:
             self.popup = Popup(title=title,
                                content=content,
-                               auto_dismiss=False,
-                               size_hint=(0.5,0.5))
+                               auto_dismiss=True,
+                               size_hint=(0.8,0.8))
         else:
             self.popup.title = title
             self.popup.content = content
         self.popup.open()
 
-    def delete_order_input(self, instance):
-        text = self.ids.Order.text
-        self.ids.Order.text = text[:-1]
-        self.Label.text = self.Label.text[:-1]
+#    def delete_order_input(self, instance):
+#        text = self.ids.Order.text
+#        self.ids.Order.text = text[:-1]
+#        self.Label.text = self.Label.text[:-1]
 
     def delete_phone_input(self, instance):
         text = self.ids.Phone.text
@@ -122,9 +127,38 @@ class CustomerApp(App):
         app = App.get_running_app()
         self.root.ids.Order.text = "HI"
 
-    def send_info(self, ordernum, phone):
-        customer_data = {str(ordernum): str(phone)}
-        self.firebase.update("new_customers", customer_data)
+    def send_info(self, phone):
+        exists = False
+#        customer_data = {str(ordernum): str(phone)}
+#        self.firebase.update("new_customers", customer_data)
+        customer_data = self.firebase.get_data("customers")
+        print(customer_data)
+        used_ids = []
+        for customer_id, customer_data in customer_data.items():
+            #check if the mobile number exists yet
+            if customer_data['mobile'] == '+65' + phone:
+                #update db
+                customer_data["served"] = False
+                self.firebase.db.child('customers').child(customer_id).update(customer_data)
+                exists = True
+                break
+            #if not,
+            else:
+                used_ids.append(customer_id)
+#        print(current_no)
+        print(used_ids)
+        new_id = None
+        if exists == False:
+            #the phone number is new
+            for i in range(1,999):
+                i = '{:0>3}'.format(str(i))
+                if i not in used_ids:
+                    new_id = i
+                    break
+            #update db
+            customer_dict = {'mobile': '+65' + phone, "served": False, 'service': "whatsapp"}
+            self.firebase.db.child('customers').child(new_id).update(customer_dict)
+
 
     def build(self):
         pass
